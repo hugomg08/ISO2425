c8053509357ecbd9a7160e7f0bdf1a6c9f258258
hugomg08 <hugomasero@iescastelar.com>
2024-11-21 10:29:19 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,17 @@
+@echo off
+rem mkprofile
+rem autor: Hugo Masero Gómez
+rem Fecha: 18-11-2024
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\dam %USERPROFILE%\ceti %USERPROFILE%\smr
+
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\examen
+
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\examen
+
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\examen
+
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\examen
+
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\examen
+
